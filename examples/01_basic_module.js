e73d1990a67c7cb0291e4c658e142c361dc7a2ac
Claus Bové <clbo@kea.dk>
2015-09-17 07:04:40 +0200

    examples added

---
+++ examples/01_basic_module.js
@@ -0,0 +1,12 @@
+var name;
+
+module.exports = {
+
+	getName : function () {
+		return name;
+	},
+	setName : function (x) {
+		name = x;
+	}
+
+}
---
+++ examples/01_basic_use_module.js
@@ -0,0 +1,4 @@
+var mod = require('./01_basic_module.js');
+
+mod.setName('Claus')
+console.log(mod.getName());
